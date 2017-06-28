83f1d8d33697f60fb1da1eaec9485cf77ea564b7
Grzegorz Szymanowski <gwszymanowski@gmail.com>
2017-06-28 20:54:02 +0200

    TreeView in group fixes

--- src/main/java/ganttchart/controller/GroupController.java
+++ src/main/java/ganttchart/controller/GroupController.java
@@ -1,15 +1,19 @@
 package ganttchart.controller;
 
+import ganttchart.model.Project;
 import ganttchart.model.ProjectGroup;
 import ganttchart.model.User;
 import ganttchart.repository.ProjectGroupRepository;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.Collection;
@@ -23,11 +27,9 @@ import java.util.ResourceBundle;
 public class GroupController implements Initializable {
 
     @FXML
-    private TreeView<String> groupList = new TreeView<String>();
-
+    private TreeView<String> groupTreeView;
     @FXML
     private Label name;
-
     @FXML
     private TextField nameField;
 
@@ -35,20 +37,32 @@ public class GroupController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         List<ProjectGroup> groups = repo.getAll();
 
-        List<TreeItem<String>> lista = new LinkedList<TreeItem<String>>();
-        for(ProjectGroup group : groups) {
-            TreeItem<String> groupItem = new TreeItem<>();
-            groupItem.setValue(group.getName());
-            groupItem.getChildren().addAll(group.getTreeItem());
-            lista.add(groupItem);
+        TreeItem<String> rootItem = new TreeItem<> ("Grupy");
+
+        for(ProjectGroup g : groups) {
+
+            TreeItem<String> rootItem1 = new TreeItem<> (g.getName());
+            rootItem1.setExpanded(true);
+
+            for(User u : g.getMembers()) {
+                TreeItem<String> item = new TreeItem<> (u.getFirstname() + " " + u.getLastname());
+                rootItem1.getChildren().add(item);
+            }
+
+            rootItem.getChildren().add(rootItem1);
         }
+        groupTreeView.setRoot(rootItem);
+    }
 
-
-       // groupList.getRoot().getChildren().addAll(lista);
-
-
+    private TreeView<String> createTreeView(TreeItem<String> root1, TreeItem<String> root2) {
+        TreeItem<String> dummyRoot = new TreeItem<>();
+        dummyRoot.getChildren().addAll(root1, root2);
+        TreeView<String> tree = new TreeView<>(dummyRoot);
+        tree.setShowRoot(true);
+        return tree ;
     }
 
     public void createProjectAction(ActionEvent actionEvent) {
